c18467c00cbddbb30cbcfe8a12beaaa98aeed43d
Jaime Rios <jaime.rios@hey.com>
2024-08-25 17:54:05 -0600

    Update package.json scripts to use 'bun run' prefix and install TailwindCSS.

--- pages/_app.js
+++ pages/_app.js
@@ -8,58 +8,59 @@ import { darkTheme, lightTheme } from "../styles/theme.config";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-    const darkMode = useDarkMode(false, { storageKey: null, onChange: null })
-    const [isMounted, setIsMounted] = useState(false)
+  const darkMode = useDarkMode(false, { storageKey: null, onChange: null })
+  const [isMounted, setIsMounted] = useState(false)
 
-    // const [theme, setTheme] = useState(lightTheme)
-    const theme = darkMode.value ? darkTheme : lightTheme;
+  // const [theme, setTheme] = useState(lightTheme)
+  const theme = darkMode.value ? darkTheme : lightTheme;
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, [])
+  useEffect(() => {
+    setIsMounted(true);
+  }, [])
 
-    return (
-        <>
-            <GoogleAnalytics />
-            <ThemeProvider theme={theme}>
-                <Head>
-                    <meta content="width=device-width, initial-scale=1" name="viewport" />
-                    <link rel="icon" href="/favicon.ico" />
+  return (
+    <>
+      <GoogleAnalytics />
+      <ThemeProvider theme={theme}>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <link rel="icon" href="/favicon.ico" />
 
-                </Head>
-                <GlobalStyle />
-                <Layout>
-                    <DefaultSeo
-                        canonical={SEO.openGraph.url}
-                        {...SEO}
-                        additionalMetaTags={[{
-                            name: 'keywords',
-                            content: SEO.openGraph.keywords,
-                        },
-                        {
-                            name: 'twitter:image',
-                            content: SEO.openGraph.images[0].url
-                        },
-                        {
-                            name: 'twitter:title',
-                            content: SEO.openGraph.title,
-                        },
-                        {
-                            name: 'twitter:description',
-                            content: SEO.openGraph.description,
-                        },
-                        {
-                            httpEquiv: 'x-ua-compatible',
-                            content: 'IE=edge; chrome=1'
-                        }]}
-                    />
-                    {isMounted && <Component {...pageProps} />}
-                </Layout>
-            </ThemeProvider>
-        </>
+        </Head>
+        <GlobalStyle />
+        <Layout>
+          <DefaultSeo
+            canonical={SEO.openGraph.url}
+            {...SEO}
+            additionalMetaTags={[{
+              name: 'keywords',
+              content: SEO.openGraph.keywords,
+            },
+            {
+              name: 'twitter:image',
+              content: SEO.openGraph.images[0].url
+            },
+            {
+              name: 'twitter:title',
+              content: SEO.openGraph.title,
+            },
+            {
+              name: 'twitter:description',
+              content: SEO.openGraph.description,
+            },
+            {
+              httpEquiv: 'x-ua-compatible',
+              content: 'IE=edge; chrome=1'
+            }]}
+          />
+          {isMounted && <Component {...pageProps} />}
+        </Layout>
+      </ThemeProvider>
+    </>
 
-    )
+  )
 }
 export default MyApp
